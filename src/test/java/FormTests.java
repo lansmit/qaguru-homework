4e0885e87c3ac70fec706b8d8c1e106a19468eed
trubnikov <trubnikov@jivosite.com>
2025-04-14 14:08:03 +0300

    Добавлен тест на проверку формы на demoqa.com

---
+++ src/test/java/FormTests.java
@@ -0,0 +1,75 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class FormTests {
+
+//    Переменные для ввода значений
+    String name = "SomeName";
+    String surname = "SomeSurname";
+    String email = "some@mail.com";
+    String phone = "9990001122";
+    String subject = "Arts";
+    String currentAddress = "St-Petersburg";
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void formSuccessfulTest() {
+//        Заполняем контакты
+        open("/automation-practice-form");
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(surname);
+        $("#userEmail").setValue(email);
+        $("#userNumber").setValue(phone);
+
+//        Выбираем пол и дату рождения
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1996");
+        $$("div.react-datepicker__day").findBy(text("11")).click();
+
+//        Выбираем предмет и хобби
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("label[for=hobbies-checkbox-1]").click();
+        $("label[for=hobbies-checkbox-2]").click();
+        $("label[for=hobbies-checkbox-3]").click();
+
+//        Загружаем картинку
+        $("#uploadPicture").uploadFromClasspath("samplephoto.jpg");
+
+//        Выбираем адрес
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#react-select-3-option-1").click();
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+        $("#submit").click();
+
+//        Проверяем выбранные значения
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
+        $$("table tr").findBy(text("Student Name")).shouldHave(text(name+" "+surname));
+        $$("table tr").findBy(text("Student Email")).shouldHave(text(email));
+        $$("table tr").findBy(text("Gender")).shouldHave(text("Male"));
+        $$("table tr").findBy(text("Mobile")).shouldHave(text(phone));
+        $$("table tr").findBy(text("Date of Birth")).shouldHave(text("11 September,1996"));
+        $$("table tr").findBy(text("Subjects")).shouldHave(text(subject));
+        $$("table tr").findBy(text("Hobbies")).shouldHave(text("Sports, Reading, Music"));
+        $$("table tr").findBy(text("Picture")).shouldHave(text("samplephoto.jpg"));
+        $$("table tr").findBy(text("Address")).shouldHave(text(currentAddress));
+        $$("table tr").findBy(text("State and City")).shouldHave(text("Uttar Pradesh Lucknow"));
+
+
+    }
+
+}

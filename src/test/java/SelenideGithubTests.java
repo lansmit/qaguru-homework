3ae593da9ee90012489953f024bb126045f38292
trubnikov <trubnikov.e.a@ya.ru>
2025-04-19 17:00:42 +0300

    Улучшен последний шаг - добавлено экранирование текста через тройные кавычки

--- src/test/java/SelenideGithubTests.java
+++ src/test/java/SelenideGithubTests.java
@@ -22,16 +22,17 @@ public class SelenideGithubTests {
         $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("Soft assertions")).click();
         // Проверяем наличие кода про JUnit 5 после заголовка "3. Using JUnit5 extend test class:"
         $$(".markdown-heading").findBy(text("3. Using JUnit5 extend test class:")).sibling(0).$("pre")
-                .shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
-                        "class Tests {\n" +
-                        "  @Test\n" +
-                        "  void test() {\n" +
-                        "    Configuration.assertionMode = SOFT;\n" +
-                        "    open(\"page.html\");\n" +
-                        "\n" +
-                        "    $(\"#first\").should(visible).click();\n" +
-                        "    $(\"#second\").should(visible).click();\n" +
-                        "  }\n" +
-                        "}"));
+                .shouldHave(text("""
+                @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }"""));
     }
 }

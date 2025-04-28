ce2691f8eee3d39d8ae56b41dfb8bc739f8bf1a5
trubnikov <trubnikov.e.a@ya.ru>
2025-04-28 21:50:47 +0300

    Убраны лишние импорты

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,10 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 

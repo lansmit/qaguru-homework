c03d62b55341c0263889d69aee9e3e68e45dc378
trubnikov <trubnikov.e.a@ya.ru>
2025-06-10 22:05:52 +0300

    Добавлены тесты на редактирование пользователя, создание пользователя, удаление пользователя, регистрацию без пароля

--- src/test/java/tests/ApiTestBase.java
+++ src/test/java/tests/ApiTestBase.java
@@ -7,7 +7,6 @@ public class ApiTestBase {
     protected static final String FREE_API_KEY_NAME = "x-api-key";
     protected static final String FREE_API_KEY_VALUE = "reqres-free-v1";
     protected static final String USERS_END_POINT = "/users/";
-    protected static final String REGISTER_END_POINT = "/register";
 
     @BeforeAll
     static void setUp() {
--- src/test/java/tests/ApiTests.java
+++ src/test/java/tests/ApiTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static java.net.HttpURLConnection.*;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class ApiTests extends ApiTestBase {
 
@@ -14,7 +14,7 @@ public class ApiTests extends ApiTestBase {
 
     @Test
     @DisplayName("Запрос данных по несуществующему пользователю")
-    void UserNotFound404Test() {
+    void userNotFound404Test() {
         given()
                 .header(FREE_API_KEY_NAME, FREE_API_KEY_VALUE)
                 .log().uri()
@@ -25,5 +25,81 @@ public class ApiTests extends ApiTestBase {
                 .statusCode(HTTP_NOT_FOUND)
                 .body(equalTo("{}"));
     }
+
+    @Test
+    @DisplayName("Редактирование пользователя")
+    void updateUserTest() {
+        given()
+                .header(FREE_API_KEY_NAME, FREE_API_KEY_VALUE)
+                .body("{\"name\": \"morpheus\", \"job\": \"zion resident\"}")
+                .log().uri()
+                .log().body()
+        .when()
+                .put(USERS_END_POINT + validUserId)
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo("morpheus"))
+                .body("job", equalTo("zion resident"))
+                .body("updatedAt", matchesRegex("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z"));
+    }
+
+    @Test
+    @DisplayName("Создание пользователя")
+    void createUserTest() {
+        given()
+                .header(FREE_API_KEY_NAME, FREE_API_KEY_VALUE)
+                .body("{\"name\": \"Skrillex\", \"job\": \"artist\"}")
+                .log().uri()
+                .log().body()
+        .when()
+                .post(USERS_END_POINT + validUserId)
+        .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("id", notNullValue())
+                .body("createdAt", matchesRegex("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z"));
+    }
+
+    @Test
+    @DisplayName("Удаление пользователя")
+    void deleteUserTest() {
+        given()
+                .header(FREE_API_KEY_NAME, FREE_API_KEY_VALUE)
+                .log().uri()
+        .when()
+                .delete(USERS_END_POINT + validUserId)
+        .then()
+                .log().status()
+                .statusCode(204);
+    }
+
+    @Test
+    @DisplayName("Регистрация без пароля")
+    void tryRegisterWithoutPasswordTest() {
+        given()
+                .header(FREE_API_KEY_NAME, FREE_API_KEY_VALUE)
+                .body("{\"email\": \"sydney@fife.qw\"")
+                .log().uri()
+                .log().body()
+       .when()
+                .post(USERS_END_POINT + validUserId)
+       .then()
+                .log().status()
+                .log().body()
+                .body("error", equalTo("Missing password"))
+                .statusCode(400);
+    }
+
+
+
+
+
+
+
+
+
 
 }
